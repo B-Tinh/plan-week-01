13edb5b2865fd3be5ecc2e78a2a861deed4c7fe1
Tịnh <winanh2151993@gmail.com>
2019-10-27 23:33:58 +0700

    arrow-function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ArrowFunction from './components/ArrowFunction';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learb acacasdca
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <ArrowFunction />
+
+      </div>
+      );
+  }
 }
 
 export default App;
