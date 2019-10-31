116151fc6f32e2fa5f13f79228d7ecf7b5899189
winanh215 <tinh.bach@terralogic.com>
2019-10-31 18:15:09 +0700

    delete plan week 1 and create plan week 2 and create component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, {Component} from 'react';
-import ArrowFunction from './components/ArrowFunction';
+import React, { Component } from 'react';
 import './App.css';
-import VarLetConst from './components/VarLetConst';
-import ShortHand from './components/ShortHand';
+import SearchBar from './components/SearchBar';
+import VideoDetail from './components/VideoDetail';
+import VideoList from './components/VideoList';
+
 
 class App extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <ArrowFunction />
-        <VarLetConst />
-        <ShortHand />
-
+        <div className="container">
+          <SearchBar />
+          <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <div className="row">
+                <VideoDetail />
+                <VideoList />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      );
+    );
   }
 }
 
