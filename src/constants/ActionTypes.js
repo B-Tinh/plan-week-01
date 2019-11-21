5ba00b68b95c868b00f1a6a178c2f639ada437b7
winanh215 <tinh.bach@terralogic.com>
2019-11-21 18:38:29 +0700

    planwk04

--- src/constants/ActionTypes.js
+++
@@ -1,6 +0,0 @@
-export const LIST_ALL = 'LIST_ALL';
-export const TOGGLE_FORM = 'TOGGLE_FORM';
-export const CLOSE_FORM = 'CLOSE_FORM';
-export const SELECT_SONG = 'SELECT_SONG';
-export const EDIT_TEXT = 'EDIT_TEXT';
-export const CHANGE_IN_TEXT = 'CHANGE_IN_TEXT';
